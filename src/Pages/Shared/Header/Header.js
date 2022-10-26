212e3385537bcb60acf5bed8592956f686cc465a
Saadi-Rahman <sadi6280@gmail.com>
2022-10-26 12:28:32 +0600

    navbar added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css';
+import navLogo from '../../../Back9.png';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar className='base-color navbar-thin'>
+                <Container>
+                    <Navbar.Text className='text-light'>
+                        backbenchers@info.com
+                    </Navbar.Text>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        <Link className='text-light text-decoration-none me-4' to="/login">Login</Link>
+                    </Navbar.Text>
+                    <Navbar.Text>
+                        <Link className='text-light text-decoration-none' to="/register">Register</Link>
+                    </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <nav className="navbar navbar-expand-lg shadow mb-5 bg-body">
+                <div className="container navbar-wide">
+                    <Link className='pb-2' to="/"><img src={navLogo} alt="" width="200px" /></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-wide-items navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => isActive ? "nav-link text-dark" : "nav-link"} to="/home">HOME</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => isActive ? "nav-link text-dark" : "nav-link"} to="courses">COURSES</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => isActive ? "nav-link text-dark" : "nav-link"} to="faq">FAQ</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => isActive ? "nav-link text-dark" : "nav-link"} to="blog">BLOG</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,46 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Blog from "../Pages/Blog/Blog";
+import CourseDetails from "../Pages/CourseDetails/CourseDetails";
+import Courses from "../Pages/Courses/Courses";
+import Faq from "../Pages/Faq/Faq";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 export const routes = createBrowserRouter([
-
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/courseDetails/:id',
+                element: <CourseDetails></CourseDetails>
+            },
+            {
+                path: '/faq',
+                element: <Faq></Faq>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+        ]
+    }
 ])
